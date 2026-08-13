3bd0621132d9e81ad6aa21a737d23782aa6a46bf
Iamtpolk <polk.t95@gmail.com>
2026-08-13 13:04:08 -0500

    Move team section earlier in homepage story

--- homepage-order.js
+++ homepage-order.js
@@ -11,10 +11,10 @@
     body.home .page-content>.md-hero{order:1!important}
     body.home .page-content>.md-listing-search{order:2!important}
     body.home .page-content>#lst-why-root{order:3!important}
-    body.home .page-content>#lst-reviews-root{order:4!important}
-    body.home .page-content>#lst-areas-root{order:5!important}
-    body.home .page-content>.md-house{order:6!important}
-    body.home .page-content>#lst-team-root{order:7!important}
+    body.home .page-content>#lst-team-root{order:4!important}
+    body.home .page-content>#lst-reviews-root{order:5!important}
+    body.home .page-content>#lst-areas-root{order:6!important}
+    body.home .page-content>.md-house{order:7!important}
     body.home .page-content>#lst-consult-root{order:8!important}
     body.home .page-content>.md-footer{order:9!important}
     body.home .page-content>.md-team-desc,
