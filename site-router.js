d43a33d5330f44f9bbfd691ae64de6efd20f5136
Iamtpolk <polk.t95@gmail.com>
2026-08-14 13:24:54 -0500

    Pin Chelsea homepage loader to verified commit

--- site-router.js
+++ site-router.js
@@ -26,7 +26,7 @@
 
   if(path==="/"){
     if(host==="chelseaporter.lakeshoreteam.com"){
-      Promise.all([load(CDN+"navbar.js?v=3"),load(CDN+"footer-v6.js"),load(CDN+"chelsea-homepage.js?v=2")]);
+      Promise.all([load(CDN+"navbar.js?v=3"),load(CDN+"footer-v6.js"),load(PIN+"28cddfadfa985f7cde65c12fcae3da5c88ca2c5e/chelsea-homepage.js")]);
     }else{
       load(PIN+"20c99e81c3ce139a173585a25f974221a1deead6/homepage-bundle.js");
     }
