93c63bd94fdea8f756259d341e150c45fdb9e191
Iamtpolk <polk.t95@gmail.com>
2026-08-26 14:51:55 -0500

    Deploy native team markup cleanup

--- site-router.js
+++ site-router.js
@@ -28,7 +28,7 @@
     if(host==="chelseaporter.lakeshoreteam.com"){
       Promise.all([load(PIN+"9f548bb89ebb9cac638ef100da2bcc78cfceed66/navbar.js"),load(CDN+"footer-v6.js"),load(CDN+"chelsea-homepage.js?v=12")]);
     }else{
-      load(PIN+"87e432093c4d1fef19105619965c0d92c3d4d20e/homepage-bundle.js");
+      load(PIN+"8fde27c1df6e7f0dc897889bf30f95d21aa25ebf/homepage-bundle.js");
     }
   }else{
     Promise.all([load(PIN+"9f548bb89ebb9cac638ef100da2bcc78cfceed66/navbar.js"),load(CDN+"footer-v6.js")]);
